e594d43293551d9c8dab8cdcdefbf11b28419a54
Phil Schaf <flying-sheep@web.de>
2026-03-10 13:01:52 +0100

    hm

--- src/anndata_plot/__init__.py
+++ src/anndata_plot/__init__.py
@@ -1,16 +1,3 @@
-from importlib.metadata import version
-
-import holoviews as hv
-
 from . import pl, pp, tl
-from .pl.utils import AnnDataInterface
 
-__all__ = ["pl"]
-
-__version__ = version("anndata-plot")
-
-# register the AnnDataInterface with holoviews
-if AnnDataInterface.datatype not in hv.core.data.datatypes:
-    hv.core.data.datatypes.append(AnnDataInterface.datatype)
-
-hv.core.Interface.register(AnnDataInterface)
+__all__ = ["pl", "pp", "tl"]
